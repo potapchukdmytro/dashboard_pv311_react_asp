e436f6977f8fbfb5fe7f8e2876ce6a7e332bb430
potapchukdmytro <traiget@gmail.com>
2025-04-30 19:13:30 +0300

    fix http

--- dashboard/src/http_common.js
+++ dashboard/src/http_common.js
@@ -4,9 +4,7 @@ const http = axios.create({
     baseURL: process.env.REACT_APP_API_URL,
     headers: {
         'Content-Type': 'application/json',
-        'Authorization': localStorage.getItem("at")
-        ? `Bearer ${localStorage.getItem("at")}` 
-        : '',
+        'Authorization': `Bearer ${localStorage.getItem("at")}`,
     }
 })
 
